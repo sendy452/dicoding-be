1ecf2426f27309e7b2aaa7ce4391fc9455cb008b
sendy452 <sendyivenyulian452@gmail.com>
2023-04-18 15:21:23 +0700

    initial

---
+++ app.js
@@ -0,0 +1,6 @@
+const nodeLocation = process.argv[0];
+const fileLocation = process.argv[1];
+const firstName = process.argv[2];
+const lastName = process.argv[3];
+ 
+console.log(`Hello (${nodeLocation}) (${fileLocation}) ${firstName} ${lastName}`);
